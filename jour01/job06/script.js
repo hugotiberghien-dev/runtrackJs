b0dcf85463c0304aadd43a37197bf31e4f198e64
hugotiberghien <hugo.tiberghien@laplateforme.io>
2026-04-22 13:55:46 +0200

    Update script.js

---
+++ jour01/job06/script.js
@@ -0,0 +1,10 @@
+function fizzbuzz() {
+    for (let i = 1; i <= 151; i++) {
+        let result = "";
+
+        if (i % 3 === 0) result += "Fizz";
+        if (i % 5 === 0) result += "Buzz";
+
+        console.log(result || i);
+    }
+}
